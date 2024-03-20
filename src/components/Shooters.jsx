0011479527d0f1dd47b2a34ffe2c06fb0c47c9ea
ShOrtyDub <rakoto.mathieu@gmail.com>
2024-03-20 22:23:12 +0100

    add input pour recherche de joueur

--- src/components/Shooters.jsx
+++ src/components/Shooters.jsx
@@ -3,40 +3,58 @@ import axios from "axios";
 import {Link} from "react-router-dom";
 
 export default function Shooters() {
-    const [players, setPlayers] = useState()
+    const [players, setPlayers] = useState([]);
+    const [search, setSearch] = useState("");
 
-    const fetchPlayers = async () => {
+    const fetchPlayers = async (name) => {
         try {
             const options = {
                 method: 'GET',
                 url: 'https://api.balldontlie.io/v1/players',
                 headers: {
                     'Authorization': '8b94bf84-fe70-4619-8d50-e307653da5fc'
-                }
+                },
+                params: {
+                    last_name: name,
+                    per_page: 100
+                },
             }
             const response = await axios(options);
             setPlayers(response.data.data);
-            console.log(response.data.data);
+            console.log(response);
         } catch (error) {
             console.error('Error fetching players:', error);
         }
     };
 
     const renderPlayers = () => {
-        if (!players) {
-            return null;
+        if (!players.length) {
+            return <div>No player found</div>;
         }
         return players.map((player) => (
-            <div key={player.id}>{player.first_name}</div>
+            <div key={player.id}>{player.first_name} {player.last_name}</div>
         ));
     }
 
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value);
+    };
+
     useEffect(() => {
-        fetchPlayers();
-    }, []);
+        if (search) {
+            fetchPlayers(search);
+        }
+    }, [search]);
 
     return (
         <Fragment>
+            <div>
+                <input type="text"
+                       placeholder="Search a player by name"
+                       value={search}
+                       onChange={handleSearchChange}
+                />
+            </div>
             <div>
                 {renderPlayers()}
             </div>
